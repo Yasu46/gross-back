2bda57f32d2691066062de37c44b83c71f7830ee
Yasu46 <y.suwa46@gmail.com>
2022-12-23 21:34:35 +0900

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "db4free.net",
+  USER: "yasu46",
+  PASSWORD: "6430613026",
+  DB: "web_gross"
+};
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "root",
+//   DB: "gross"
+// };
